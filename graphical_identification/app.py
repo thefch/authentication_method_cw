dc516daf2828442d380c94fa420837cdf68437c8
Fanos <theofanis.ch@hotmail.com>
2020-02-11 13:26:34 +0000

    initial commit

---
+++ graphical_identification/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+@app.route('/')
+def index():
+    rsp = (render_template("index.html"))
+
+
+    return rsp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
